e123414b924d146a5563fd635f288c882534f8e1
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2023-04-29 17:55:26 -0400

    Create languages.py

---
+++ neuronal/languages.py
@@ -0,0 +1,14 @@
+langs={
+	"01":"binario",
+  "es-VE":"Spanish - Venezuela",
+  "ru":"Russian",
+	"uk":"Ukrainian",
+  "chi":"creahachipne",
+  "ja":"Japanese",
+  "zh":"Chinese ",
+  "ar":"Arabic",
+  "en":"English",
+  "ca":"Catalan",
+  "it":"italiano"
+    
+	
